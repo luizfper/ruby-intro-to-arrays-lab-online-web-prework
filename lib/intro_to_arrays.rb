830ea0f74769be910898872d05b685268283b0e1
Luiz Pereira <luizfper@gmail.com>
2019-01-29 18:27:57 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,21 @@
+def instantiate_new_array
+  array=[]
+end
+
+def array_with_two_elements
+  array=["cao","cachorro"]
+end
+
+def first_element(array)
+  array.first
+end
+
+def third_element(array)
+  array[3]
+end
+
+def last_element(array)
+  array[-1]
+end
+
+def
